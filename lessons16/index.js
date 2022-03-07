d1f2a65f6556b9d5eceeda76a836487902539b19
Evgeniy0258 <yachnik-98@mail.ru>
2022-03-07 08:41:14 +0300

    feat: add index.html and index.js

---
+++ lessons16/index.js
@@ -0,0 +1,88 @@
+'use strict'
+
+// let admin;
+// let UserName = "Jon";
+// admin = UserName;
+// alert(admin);
+
+
+// let PlanetEarth;
+// let UserName;
+
+
+// let x = 20;
+// let y = 58;
+// let z = 42;
+// let summa = x + y + z;
+// console.log(summa);
+
+
+// let secund = 60;
+// let minute = 60;
+// let hour = 24;
+// let day = 365;
+// let age = 23;
+// age = secund;
+// let myAgeInSeconds = secund * minute * hour * day;
+// alert(myAgeInSeconds);
+
+
+// let count = 42;
+// let userName = '42';
+// count = String(count);
+// userName = Number(userName);
+// console.log(count, userName);
+
+// let a = 1
+// let b = 2
+// let c = "белых медведей";
+// console.log(`${a}${b} ${c}`);
+
+
+// let a = "access";
+// let b = "marine";
+// let c = "frost";
+// let d = "reproach";
+// let e = "chopped";
+
+// var lengthWords = (a.length + b.length + c.length + d.length + e.length);
+// console.log(lengthWords);
+
+
+// let a = '5';
+// let b = "text";
+// let c = true;
+// let at = typeof (a);
+// let bt = typeof (b);
+// let ct = typeof (c);
+// console.log(`Variable:have type: ${at} Variable:have type:${bt} Variable:have type:${ct} `)
+
+
+
+let UserName = prompt('как тебя зовут?');
+let UserAge = prompt('Сколько тебе лет?');
+alert(`Тебя зовут: ${UserName} 
+ тебе ${UserAge} лет`);
+
+
+
+// let a = 4;
+// let b = 3;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(`${a} ${b}`)
+
+
+// let codeWord1 = "обернись";
+// let codeWord2 = "неужели";
+// let codeWord3 = "огурцы";
+// let codeWord4 = "липкие";
+// let codeWord5 = "?!";
+// let a = codeWord1[1];
+// let b = codeWord2[1];
+// let c = codeWord3[1];
+// let d = codeWord4[1];
+// let e = codeWord5[1];
+// console.log(`${a} ${b} ${c} ${d} ${e}`)
+
