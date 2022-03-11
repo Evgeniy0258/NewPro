5de2da4ed40af3ebceee6f827c1aea9894af6a79
Evgeniy0258 <yachnik-98@mail.ru>
2022-03-11 19:01:42 +0300

    feat: add task/Lessons17

---
+++ feature/HW-17/index.js
@@ -0,0 +1,87 @@
+// let first = 'true';
+// let second = false;
+// let third = 17;
+// let fourth = undefined;
+// let fifth = null;
+// console.log(typeof(first), typeof(second), typeof(third), typeof(fourth), typeof(fifth));
+
+
+// let height = 15;
+// let width = 20;
+
+// if (height < width) {
+//     console.log(width)
+// }
+// else {
+//     console.log(height);
+// }
+
+
+// let b = 20;
+
+// for (let a = 1; a < b; a += 1) {
+//     if (a % 3 !== 0) {
+//         continue;
+//     }
+//     console.log(a);
+// }
+
+
+// let key = true;
+// let documents = true;
+// let pen = true;
+// let apple = false;
+// let orange = true;
+// let shoulGoToWork;
+// if ((key == true && documents == true && pen == true) && (apple == true || orange == true)) {
+//     shoulGoToWork = true;
+// } else {
+//     shoulGoToWork = false;
+// }
+// console.log(shoulGoToWork);
+
+
+// let n = +prompt();
+// if ((n % 3 != 0) && (n % 5 == 0)) {
+//     console.log('Fiz');
+// } else if ((n % 3 == 0) && (n % 5 != 0)) {
+//     console.log('Buz');
+// } else if ((n % 3 == 0) && (n % 5 == 0)) {
+//     console.log('FizBuz');
+// }
+
+
+
+// let age = +prompt('Возраст?');
+// if (18 <= age) {
+//     alert('Выпей пивка');
+// } else if (16 <= age) {
+//     alert('покури сигаретку, только тссс');
+// } else if (15 >= age) {
+//     alert('увы, только кола');
+// }
+
+
+// let direction = prompt('в какую сторону света ты бы хотел отправиться?')
+// switch (direction) {
+//     case 'юг': {
+//         alert('на юг пойдешь счастье найдешь');
+//         break;
+//     }
+//     case 'север': {
+//         alert('на север пойдешь много денег найдешь');
+//         break;
+//     }
+//     case 'запад': {
+//         alert('на запад пойдешь верного друга найдешь');
+//         break;
+//     }
+//     case 'восток': {
+//         alert('на восток пойдешь разработчиком станешь');
+//         break;
+//     }
+//     default: {
+//         alert('еще раз')
+//         break;
+//     }
+// }
