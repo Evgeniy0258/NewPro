d37bb5a1fbfc22e7be6c16a3280b5ac8f7a15688
Evgeniy0258 <yachnik-98@mail.ru>
2022-03-11 19:20:26 +0300

    refactor:  correcting errors in the task/Lessons16

--- lessons16/index.js
+++ lessons16/index.js
@@ -6,8 +6,8 @@
 // alert(admin);
 
 
-// const PlanetEarth;
-// const userName;
+// let PlanetEarth;
+// let UserName;
 
 
 // let x = 20;
@@ -17,13 +17,13 @@
 // console.log(summa);
 
 
-// let seconds_in_minutes = 60;
-// let minute_in_hour = 60;
-// let hour_in_day = 24;
-// let day_in_year = 365;
+// const SECONDS_IN_MINUTES = 60;
+// const MINUTES_IN_HOUR = 60;
+// const HOURS_IN_DAY = 24;
+// const DAYS_IN_YEAR = 365;
 // let age = 23;
 // age = seconds_in_minutes;
-// let myAgeInSeconds = seconds_in_minutes * minute_in_hour * hour_in_day * day_in_year;
+// const myAgeInSeconds = age * SECONDS_IN_MINUTES * MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_YEAR;
 // alert(myAgeInSeconds);
 
 
@@ -33,9 +33,9 @@
 // userName = Number(userName);
 // console.log(count, userName);
 
-// const a = 1
-// const b = 2
-// const c = "белых медведей";
+// let a = 1
+// let b = 2
+// let c = "белых медведей";
 // console.log(`${a}${b} ${c}`);
 
 
@@ -45,24 +45,24 @@
 // let d = "reproach";
 // let e = "chopped";
 
-// let lengthWords = (a + b + c + d + e).length;
+// var lengthWords = (a.length + b.length + c.length + d.length + e.length);
 // console.log(lengthWords);
 
 
 // let a = '5';
 // let b = "text";
 // let c = true;
-// let aType = typeof (a);
-// let bType = typeof (b);
-// let cType = typeof (c);
+// let at = typeof (a);
+// let bt = typeof (b);
+// let ct = typeof (c);
 // console.log(`Variable:have type: ${at} Variable:have type:${bt} Variable:have type:${ct} `)
 
 
 
-// let UserName = prompt('как тебя зовут?');
-// let UserAge = prompt('Сколько тебе лет?');
-// alert(`Тебя зовут: ${UserName}
-//  тебе ${UserAge} лет`);
+let UserName = prompt('как тебя зовут?');
+let UserAge = prompt('Сколько тебе лет?');
+alert(`Тебя зовут: ${UserName} 
+ тебе ${UserAge} лет`);
 
 
 
