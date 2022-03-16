1d55fd2cb244ff6587b3defdec285f39fba12d7a
Evgeniy0258 <yachnik-98@mail.ru>
2022-03-16 18:58:07 +0300

    feat:add taskHW-18

---
+++ feature/Hw-18/index.js
@@ -0,0 +1,56 @@
+// let n = prompt();
+// function getSum(n) {
+//     let sum = 0;
+//     for (let a = 1; a <= n; a += 1) {
+//     sum += a;
+//     }
+//     return sum;
+// }
+// alert(getSum(n));
+
+
+
+
+// let n = prompt();
+// function credit(n) {
+//     result = n * (0.17 * 5)
+//     return result;
+// }
+// alert(credit(n));
+
+
+// function trimString(str, x, y) {
+//     return console.log(str.substr(x, y));
+// }
+// alert(trimString('hshsjjdhahj', 1, 3));
+
+// function getSumNumbers(n) {
+//     let suma = 0;
+//     while (n > 0) {
+//         suma = suma + (n % 10);
+//         n = Math.floor(n / 10);
+//     }
+//     return suma;
+// }
+// alert(getSumNumbers(2021));
+
+// function getSum(a, b) {
+//     return a === b ? a : a + getSum(a + 1, b)
+// }
+// alert(getSum(1, 2));
+
+
+// function triangle(a, b, c) {
+//     if (a + b > c) {
+//         return true;
+//     } else if (a + c > b) {
+//         return true;
+//     } else if (b + c > a) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// alert(triangle(3, 4, 5));
+
+
