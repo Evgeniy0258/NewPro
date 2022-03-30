ea7972d431fc4fe53bec21866ce6aa2cb27080d6
Evgeniy0258 <yachnik-98@mail.ru>
2022-03-30 17:23:59 +0300

    feat: add HW-21

---
+++ feature/HW-21/index.js
@@ -0,0 +1,44 @@
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+// fibonacci.forEach(element => console.log(element));
+// function qwe(v) {
+//     console.log(v);
+// }
+// fibonacci.forEach(qwe);
+
+// const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton'];
+// const newUsers = users.map((v, i) => v = 'member ' + (i + 1) + ': ' + v);
+// console.log(newUsers);
+// function newName(v, i) {
+//     result = 'member ' + (i + 1) + ': ' + v;
+//     return result;
+// }
+// const newName2 = users.map(newName);
+// console.log(newName2);
+
+// const numbers = [7, -4, 32, -90, 54, 32, -21];
+// const result = numbers.filter((v) => (v > 0));
+// console.log(result);
+// function newNumbers(v) {
+//     if (v > 0)
+//         return v;
+// }
+// const newNumbers2 = numbers.filter(newNumbers);
+// console.log(newNumbers2);
+
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+// const result = fibonacci.reduce((v, sum) => sum + v);
+// console.log(result);
+// function fibonacci2(v, sum) {
+//     return sum + v
+// }
+// const result2 = fibonacci.reduce(fibonacci2);
+// console.log(result2);
+
+// const numbers = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5];
+// const even = numbers.find((v) => v % 2 === 0);
+// console.log(even);
+// function result(v) {
+//     if (v % 2 === 0) return v;
+// }
+// const even2 = numbers.find(result);
+// console.log(even2);
